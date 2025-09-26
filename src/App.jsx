543c6509c0f4fb1819cb5d2877207b59805fa9e9
ky7408 <ky7408@naver.com>
2025-09-27 08:59:39 +0900

    홈 컴포넌트,헤더 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Post from "./pages/Post";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
         <Route path="post" element={<Post />} />
       </Route>
     </Routes>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/imgs/logo.png";
+import "./Header.scss";
 
 const Header = () => {
-  return <div>헤더</div>;
+  const isActive = true;
+  return (
+    <div className="header_area">
+      <div>
+        <Link to="/">
+          <img src={logo} alt="로고" />
+        </Link>
+      </div>
+      <Link to="/post">
+        <button className={`logo_btn txt-16-b ${isActive ? "active" : ""}`}>
+          롤링 페이퍼 만들기
+        </button>
+        {/* <Button text={"롤링 페이퍼 만들기"} className={`logo_btn txt-16-b ${isActive ? "active" : ""}`}/>  버튼 수정 예정*/}
+      </Link>
+    </div>
+  );
 };
 
 export default Header;
