b7327003a3eb2a4058ece66e44498629d797b285
DESKTOP-KDUIRM0\jukma <jukman81@gmail.com>
2025-09-27 22:04:17 +0900

    css 중복 적용 수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App.jsx";
+import "@/assets/css/_root.scss";
+import "@/assets/css/_reset.scss";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
