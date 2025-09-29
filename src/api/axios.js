c373202773509ba325062e27f75d76e0e6ed468a
yujin-fe <hyujin1224@gmail.com>
2025-09-29 15:40:03 +0900

    Rename:axios baseURL 변경

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://rolling-api.vercel.app/19-4/",
+  baseURL: "https://rolling-api.vercel.app/",
 });
 
 export default instance;
