269b52d23a5d998f722165e50ca953eb56bc913e
DESKTOP-KDUIRM0\jukma <jukman81@gmail.com>
2025-09-29 15:11:44 +0900

    css 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "@/assets/css/_root.scss";
 import "@/assets/css/_reset.scss";
+import "@/assets/css/_style.scss";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
