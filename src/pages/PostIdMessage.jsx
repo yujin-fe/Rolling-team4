501b17a0a8f2b49bd2a7e3d8a77f48d5db5b8d28
DESKTOP-KDUIRM0\jukma <jukman81@gmail.com>
2025-09-30 16:11:23 +0900

    postidmessage 작업중

--- src/pages/PostIdMessage.jsx
+++ src/pages/PostIdMessage.jsx
@@ -1,10 +1,31 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import "./PostIdMessage.scss";
+import InputPost from "../components/InputPost";
 
 const PostIdMessage = () => {
+  const [sender, setSender] = useState("");
+
   const params = useParams();
-  return(
-    <div>{params.id}번 포스트에 작성될 카드</div>
-  )
-}
+  const isDisabled = !sender.trim();
+
+  const handleGenerate = async () => {};
+  return (
+    <div className="post-messase-wrap">
+      <div>{params.id}번 포스트에 작성될 카드</div>
+      <div className="post-container">
+        <InputPost title="To." value={sender} onChange={setSender} />
+      </div>
+      <button
+        type="button"
+        className="btn full"
+        onClick={handleGenerate}
+        disabled={isDisabled}
+      >
+        생성하기
+      </button>
+    </div>
+  );
+};
 
 export default PostIdMessage;
