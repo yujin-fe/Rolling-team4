c12e90b51844c77ceb9c56eae9df69006a015e7e
yujin-fe <hyujin1224@gmail.com>
2025-10-01 10:37:46 +0900

    Refactor: lint 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
+import "@/assets/css/_root.scss";
+import "@/assets/css/_reset.scss";
+import "@/assets/css/_style.scss";
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App.jsx";
-import "@/assets/css/_root.scss";
-import "@/assets/css/_reset.scss";
-import "@/assets/css/_style.scss";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
