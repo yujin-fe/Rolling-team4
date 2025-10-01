84b586f9cf54b2e5af7f96d910b9abc9b330b634
le2yunji <leeyunji00711@gmail.com>
2025-10-01 16:20:20 +0900

    git stash pop
    git push origin feature/button

---
+++ src/pages/Post.jsx
@@ -0,0 +1,57 @@
+import "./Post.scss";
+import { useState } from "react";
+
+import BackgroundSelect from "../components/BackgroundSelect";
+import InputPost from "../components/InputPost";
+
+import axios from "@/api/axios";
+
+const Post = () => {
+  const [receiver, setReceiver] = useState("");
+  const [background, setBackground] = useState("");
+  const [tab, setTab] = useState("color");
+
+  const isDisabled =
+    !receiver.trim() ||
+    (tab === "color" && !background) ||
+    (tab === "image" && !background);
+
+  const handleGenerate = async () => {
+    const payload = {
+      team: "4",
+      name: receiver,
+      backgroundColor: tab === "color" ? background : "beige",
+      backgroundImageURL: tab === "image" ? background : null,
+    };
+    try {
+      const res = await axios.post("/4/recipients/", payload);
+      console.log("등록성공", res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="post-wrap">
+      <div className="post-container">
+        <InputPost title="To." value={receiver} onChange={setReceiver} />
+        <BackgroundSelect
+          tab={tab}
+          setTab={setTab}
+          background={background}
+          setBackground={setBackground}
+        />
+      </div>
+      <button
+        type="button"
+        className="btn full"
+        onClick={handleGenerate}
+        disabled={isDisabled}
+      >
+        생성하기
+      </button>
+    </div>
+  );
+};
+
+export default Post;
