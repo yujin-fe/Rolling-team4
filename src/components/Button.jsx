075fb78480015197fe5f4773786053880aedb665
le2yunji <leeyunji00711@gmail.com>
2025-10-01 16:44:01 +0900

    버튼

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,23 @@
-import "./Button.scss";
-import React from "react";
+/*
+    <Button
+      icon={icon}            // 있다면 icon 이미지 임포트해서 쓰기
+      variant="primary"      // primary / secondary / outlined  
+      size="full"            // full / lg / wd / md / sm
+      onClick={}             // onClick 함수 
+      disabled={isDisabled}  // disabled 조건 함수
+    >
+      생성하기
+    </Button>
+*/
+
+import "../assets/css/_style.scss";
 
 function Button({ variant = "primary", size, icon, children, ...props }) {
-  const classNames = ["btn", variant, size, icon ? "with-icon" : ""].join(" ");
+  const classNames = ["btn", variant, size, icon ? "has-icon" : ""].join(" ");
 
   return (
     <button className={classNames} {...props}>
-      {icon && <img src={icon} alt="" className="btn-icon" />}
+      {icon && <img src={icon} alt="" className="has-icon" />}
       {children}
     </button>
   );
