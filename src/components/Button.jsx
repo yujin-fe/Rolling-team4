56712ff49b702a644bc7a02293ecf59d6be11012
DESKTOP-KDUIRM0\jukma <jukman81@gmail.com>
2025-10-01 14:21:01 +0900

    post/message 작업중

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import "./Button.scss";
+import React from "react";
 
 function Button({ variant = "primary", size, icon, children, ...props }) {
   const classNames = ["btn", variant, size, icon ? "with-icon" : ""].join(" ");
