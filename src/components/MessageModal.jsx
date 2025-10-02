c8a13b34bb147bf6fe44e877f686b0f317ec2b96
DESKTOP-KDUIRM0\jukma <jukman81@gmail.com>
2025-10-02 10:38:27 +0900

    날짜 포맷팅 추가

--- src/components/MessageModal.jsx
+++ src/components/MessageModal.jsx
@@ -14,7 +14,9 @@ const MessageModal = ({ data, onClose }) => {
           </div>
           <span className="relationship">{relationship}</span>
         </div>
-        <span className="createdAt txt-14">{createdAt}</span>
+        <span className="createdAt txt-14">
+          {new Date(createdAt).toLocaleDateString("ko-KR")}
+        </span>
       </div>
 
       <div className="body">
