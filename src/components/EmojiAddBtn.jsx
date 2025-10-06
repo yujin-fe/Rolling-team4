0608943f8ad1f971a7321e7b9c61128029ff2e69
yujin-fe <hyujin1224@gmail.com>
2025-10-06 17:26:42 +0900

    feat:이모지라이브러리 추가

--- src/components/EmojiAddBtn.jsx
+++ src/components/EmojiAddBtn.jsx
@@ -1,14 +1,24 @@
+import "./EmojiAddBtn.scss";
+import { EmojiPicker } from "frimousse";
+
 import emojiAdd from "../assets/icons/emoji-add.png";
 
-import Button from "./Button"
+import Button from "./Button";
 
 const EmojiAddBtn = () => {
-
-  return(
-    <Button icon={emojiAdd} size="sm" variant="outlined">
-      추가
-    </Button>
-  )
-}
+  return (
+    <div className="EmojiAddBtn">
+      <Button icon={emojiAdd} size="sm" variant="outlined">
+        추가
+      </Button>
+      <EmojiPicker.Root locale="ko" columns={8} onEmojiSelect={({ emoji }) => console.log(emoji)}>
+        <EmojiPicker.Search />
+        <EmojiPicker.Viewport>
+          <EmojiPicker.List />
+        </EmojiPicker.Viewport>
+      </EmojiPicker.Root>
+    </div>
+  );
+};
 
 export default EmojiAddBtn;
