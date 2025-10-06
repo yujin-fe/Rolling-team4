6bce3e89d4c5adbcf38a9a3fa0423a4bf251a9b5
le2yunji <leeyunji00711@gmail.com>
2025-10-06 16:16:33 +0900

    메시지 카드 리팩토링

--- src/pages/PostId.jsx
+++ src/pages/PostId.jsx
@@ -1,12 +1,14 @@
+import { Suspense } from "react";
 import { useParams } from "react-router-dom";
 import MessageCard from "../components/MessageCard";
 
 const PostId = () => {
   const params = useParams();
+
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <MessageCard recipientId={params.id} />
-    </>
+    </Suspense>
   );
 };
 
