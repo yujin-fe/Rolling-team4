d75b175dcbb83a181c478d912f116e9f9ab16d27
yujin-fe <hyujin1224@gmail.com>
2025-10-14 18:35:12 +0900

    fix:카카오톡 공유주소 변경

--- src/components/ShareBtn.jsx
+++ src/components/ShareBtn.jsx
@@ -21,16 +21,16 @@ const ShareBtn = ({ recipientData, recipientId, onClickShareBtn, isOpened }) => 
           recipientData.backgroundImageURL ??
           `https://cdn.enewstoday.co.kr/news/photo/201706/1069327_235136_2726.jpg`,
         link: {
-          webUrl: `https://part2-team4-project.vercel.app/post/${recipientId}`,
-          mobileWebUrl: `https://part2-team4-project.vercel.app/post/${recipientId}`,
+          webUrl: `https://rolling-team4.vercel.app//post/${recipientId}`,
+          mobileWebUrl: `https://rolling-team4.vercel.app/post/${recipientId}`,
         },
       },
       buttons: [
         {
           title: "웹으로 이동",
           link: {
-            webUrl: `https://part2-team4-project.vercel.app/post/${recipientId}`,
-            mobileWebUrl: `https://part2-team4-project.vercel.app/post/${recipientId}`,
+            webUrl: `https://rolling-team4.vercel.app/post/${recipientId}`,
+            mobileWebUrl: `https://rolling-team4.vercel.app/post/${recipientId}`,
           },
         },
       ],
